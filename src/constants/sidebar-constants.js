7629cb1d5c3f74f7849f2a3b9c046fa728c345c1
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2019-05-09 11:41:35 -0400

    View tooltips

--- src/constants/sidebar-constants.js
+++ src/constants/sidebar-constants.js
@@ -3,5 +3,7 @@ export const TOOLTIP_IDS = Object.freeze({
   CREATE_COLLECTION: 'create-collection',
   CREATE_DATABASE_BUTTON: 'create-database-button',
   DROP_COLLECTION: 'drop-collection',
-  DROP_DATABASE: 'drop-database'
+  DROP_DATABASE: 'drop-database',
+  DROP_VIEW: 'drop-view',
+  MODIFY_VIEW: 'modify-view'
 });
