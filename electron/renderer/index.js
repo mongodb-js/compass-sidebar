b2b0778375478014a88cbaf70d6fd5e6b0281d6c
Alena Khineika <alena.khineika@gmail.com>
2019-10-16 15:46:59 +0200

    fix: return ssh attrs

--- electron/renderer/index.js
+++ electron/renderer/index.js
@@ -82,6 +82,8 @@ const state = { instance: global.hadronApp.instance };
 DeploymentStateStore.setToInitial();
 appRegistry.emit('data-service-initialized', dataService);
 dataService.connect((error, ds) => {
+  ds.client.model.sshTunnel = 'USER_PASSWORD';
+  ds.client.model.sshTunnelHostname = '123.45.67.89';
   appRegistry.emit('data-service-connected', error, ds);
   appRegistry.emit('instance-refreshed', state);
 });
